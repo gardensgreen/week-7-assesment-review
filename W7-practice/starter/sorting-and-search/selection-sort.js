6a459f093700f03016966574e34e5d5b7990c0a4
Daniel TIllero <danieltillero67@gmail.com>
2020-09-27 22:02:50 -0400

    selection sort implementation

--- W7-practice/starter/sorting-and-search/selection-sort.js
+++ W7-practice/starter/sorting-and-search/selection-sort.js
@@ -1,4 +1,19 @@
-function selectionSort(arr) {}
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length - 1; i++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            swap(arr, min, i);
+        }
+    }
+    return arr;
+}
 
 // A swap helper function may be useful to implement the sort function above
-function swap(arr, index1, index2) {}
+function swap(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
