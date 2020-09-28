f950c1f2ce03a3c9929650ea3b2b64c2551065a6
Daniel TIllero <danieltillero67@gmail.com>
2020-09-27 22:18:05 -0400

    insertion sort implementation

--- W7-practice/starter/sorting-and-search/insertion-sort.js
+++ W7-practice/starter/sorting-and-search/insertion-sort.js
@@ -1,1 +1,12 @@
-function insertionSort(arr) {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length - 1; i++) {
+        let currentValue = arr[i];
+        for (let j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentValue;
+    }
+    return arr;
+}
+
+currentValue = 1;
